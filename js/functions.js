7e374365417dc121ec68c533ec6f43c2fe3afcbd
Rafael S. Meneses <meneses.tio@gmail.com>
2014-09-08 12:55:47 -0300

    Alteração de layout
    Inserido framework css e corrigido bugs

--- js/functions.js
+++ js/functions.js
@@ -1,85 +1,168 @@
 /** Função para conversão de número 
   * decimal para número binário
-**/
-function decForBin(numero) {
-         for ( binário = []; numero > 1; ) {
-                resto = Math.floor( numero % 2 );
-                numero /= 2;
-                binário.unshift( resto == 1 ? 1 : 0 );
-        }
- 
-        if ( !/[^0]/.test( binário.join('') ) ) binário.unshift( 1 );
- 
-        return binário.join('');
-}
+  **/
+  function decForBin(numero) {
+  	for ( binario = []; numero > 1; ) {
+  		resto = Math.floor( numero % 2 );
+  		numero /= 2;
+  		binario.unshift( resto == 1 ? 1 : 0 );
+  	}
+
+  	if ( !/[^0]/.test( binario.join('') ) ) binario.unshift( 1 );
+
+  	return binario.join('');
+  }
 
 /** Função para conversão de número 
   * binário para número decimal
-**/
-function binForDec(numero) {
-        var base2 = [],
-            decimal = 0,
-            binário = numBinário.toString();
- 
-        for ( i = binário.length, j = 1; --i > -1; j *= 2 ) base2.unshift( j );
- 
-        for ( i in binário ) {
-                if ( binário[i] == 0 ) delete base2[i];
-                else decimal += base2[i];
-        }
- 
-        return decimal;
-}
+  **/
+  function binForDec(numero) {
+  	var base2 = [],
+  	decimal = 0,
+  	binario = numero.toString();
 
-/** Função para conversão de número 
-  * decimal para número hexadecimal
-**/
-function decForHex(numero) {
-	return numero.toString(16);
-}
+  	for ( i = binario.length, j = 1; --i > -1; j *= 2 ) base2.unshift( j );
 
-/** Função para conversão de número 
-  * hexadecimal para número decimal
-**/
-function hexForDec(numero) {
-	return parseInt(numero,16);
-}
+  		for ( i in binario ) {
+  			if ( binario[i] == 0 ) delete base2[i];
+  			else decimal += base2[i];
+  		}
+
+  		return decimal;
+  	}
 
 /** Função para conversão de número 
   * binário para número hexadecimal
-**/
-function binForHex(numero) {
-	var dec = binForDec(numero);
-	return decForHex(dec);
-}
+  **/
+  function binForHex(numero) {
+  	var dec = binForDec(numero);
+  	return decForHex(dec);
+  }
 
 /** Função para conversão de número 
   * hexadecimal para número decimal
-**/
-function hexForBin(numero) {
-	var dec = hexForDec(numero);
-	return decForBin(dec);
+  **/
+  function hexForBin(numero) {
+  	var dec = hexForDec(numero);
+  	return decForBin(dec);
+  }
+
+/** Função para conversão de número 
+  * hexadecimal para número decimal
+  **/
+  function hexForDec(numero) {
+  	return parseInt(numero,16);
+  }
+
+/** Funções para conversão de número 
+  * decimal para número hexadecimal
+  **/
+  function decForHex(numero) {
+  	return tohex(numero);
+  }
+
+  function tohex(i) {
+  	a2 = ''
+  	ihex = hexQuot(i);
+  	idiff = eval(i + '-(' + ihex + '*16)')
+  	a2 = itohex(idiff) + a2;
+  	while( ihex >= 16) {
+  		itmp = hexQuot(ihex);
+  		idiff = eval(ihex + '-(' + itmp + '*16)');
+  		a2 = itohex(idiff) + a2;
+  		ihex = itmp;
+  	} 
+  	a1 = itohex(ihex);
+  	return a1 + a2 ;
+  }
+
+  function hexQuot(i) {
+  	return Math.floor(eval(i +'/16'));
+  }
+
+  function itohex(i) {
+  	if( i == 0) {
+  		aa = '0' }
+  		else { if( i== 1) {
+  			aa = '1'}
+  			else {if( i== 2) {
+  				aa = '2'}
+  				else {if( i == 3) {
+  					aa = '3' }
+  					else {if( i== 4) {
+  						aa = '4'}
+  						else {if( i == 5) {
+  							aa = '5' }
+  							else {if( i== 6) {
+  								aa = '6'}
+  								else {if( i == 7) {
+  									aa = '7' }
+  									else {if( i== 8) {
+  										aa = '8'}
+  										else {if( i == 9) {
+  											aa = '9'}
+  											else {if( i==10) {
+  												aa = 'A'}
+  												else {if( i==11) {
+  													aa = 'B'}
+  													else {if( i==12) {
+  														aa = 'C'}
+  														else {if( i==13) {
+  															aa = 'D'}
+  															else {if( i==14) {
+  																aa = 'E'}
+  																else {if( i==15) {
+  																	aa = 'F'}
+
+  																}
+  															}
+  														}
+  													}
+  												}
+  											}
+  										}
+  									}
+  								}
+  							}
+  						}
+  					}
+  				}
+  			}
+  		}
+  		return aa
+  	}
+
+
+  	$('#convert').click(function(){
+  		var opt1 = $("input[name='opt1']:checked").val();
+  		var opt2 = $("input[name='opt2']:checked").val();
+  		var num   = $('#num').val();
+
+  		if(opt1 == 'Dec' && opt2 == 'Hex'){
+  			popup('Resultado', decForHex(num));
+  		}else if(opt1 == 'Hex' && opt2 == 'Dec'){
+  			popup('Resultado', hexForDec(num));
+  		}else if(opt1 == 'Dec' && opt2 == 'Bin'){
+  			popup('Resultado', decForBin(num));
+  		}else if(opt1 == 'Bin' && opt2 == 'Dec'){
+  			popup('Resultado', binForDec(num));
+  		}else if(opt1 == 'Bin' && opt2 == 'Hex'){
+  			popup('Resultado', binForHex(num));
+  		}else if(opt1 == 'Hex' && opt2 == 'Bin'){
+  			popup('Resultado', hexForBin(num));
+  		}
+
+  	});
+
+///////////////////
+// Initialize Popup
+///////////////////
+function popup(title, message){
+	$.UIPopup({
+		id: "warning",
+		title: title, 
+		message: message,  
+		continueButton: 'Ok', 
+		callback: $.noop
+	});
 }
-
-$('#convert').click(function(){
-	var opt1 = $('#opt1').val();
-	var opt2 = $('#opt2').val();
-	var num   = $('#num').val();
-
-	alert(decForBin(num));
-
-	if(opt1 == 'Dec' && opt2 == 'Hex'){
-		alert(decForHex(num));
-	}else if(opt1 == 'Hex' && opt2 == 'Dec'){
-		alert(hexForDec(num));
-	}else if(opt1 == 'Dec' && opt2 == 'Bin'){
-		alert(decForBin(num));
-	}else if(opt1 == 'Bin' && opt2 == 'Dec'){
-		alert(binForDec(num));
-	}else if(opt1 == 'Bin' && opt2 == 'Hex'){
-		alert(binForHex(num));
-	}else if(opt1 == 'Hex' && opt2 == 'Bin'){
-		alert(hexForBin(num));
-	}
-
-});
